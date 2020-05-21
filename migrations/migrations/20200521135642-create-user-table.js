2f8a669a2af3419d49a25352584bc3d1a1fd8b86
Julián Alvarez <alvarezjulianignacio@gmail.com>
2020-05-21 11:10:38 -0300

    user model and table migration

---
+++ migrations/migrations/20200521135642-create-user-table.js
@@ -0,0 +1,23 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) =>
+    queryInterface.createTable(
+      'users',
+      {
+        id: { type: Sequelize.INTEGER, autoIncrement: true, allowNull: false },
+        name: { type: Sequelize.STRING, allowNull: false },
+        last_name: { type: Sequelize.STRING, allowNull: false },
+        email: { type: Sequelize.STRING, allowNull: false, unique: true },
+        password: {
+          type: Sequelize.STRING,
+          required: true
+        }
+      },
+      {
+        timestamps: false
+      }
+    ),
+
+  down: queryInterface => queryInterface.dropTable('users')
+};
